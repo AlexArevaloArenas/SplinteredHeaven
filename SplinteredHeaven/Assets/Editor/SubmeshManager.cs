1262cd69183b5ee0f02c694d27ae6a7a3957d3fc
Floromakoba <99040080+Floromakoba@users.noreply.github.com>
2025-06-04 18:24:33 +0200

    Submesh tool arreglada (espero)

--- SplinteredHeaven/Assets/Editor/SubmeshManager.cs
+++ SplinteredHeaven/Assets/Editor/SubmeshManager.cs
@@ -1,10 +1,12 @@
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.LowLevelPhysics;
 
 public class SubmeshManager : EditorWindow
 {
     
     Mesh mesh;
+    int[] triangles;
     [MenuItem("Tools/Submesh Manager")]
     public static void ShowWindow()
     {
@@ -16,13 +18,32 @@ public class SubmeshManager : EditorWindow
         GUILayout.Label("Add or remove submeshes", EditorStyles.boldLabel);
 
         mesh = EditorGUILayout.ObjectField(mesh, typeof(Mesh), false) as Mesh;
+
         if (mesh != null)
         {
             Debug.Log("Mesh: " + mesh.name);
             if (GUILayout.Button("Add New Submesh")) GenerateSubMesh();
             if (GUILayout.Button("Remove Last Submesh")) RemoveSubMesh();
+            //if (GUILayout.Button("View Info")) ViewInfo();
+        }
+
+        GUILayout.Label("Submesh Count: " + mesh.subMeshCount);
+        for (int i = 0; i< mesh.subMeshCount; i++)
+        {
+            triangles = mesh.GetTriangles(i);
+            GUILayout.Label($"Submesh {i} has {triangles.Length / 3} triangles.");
         }
     }
+
+    /*void ViewInfo()
+    {
+        Debug.Log("Submesh Count: " + mesh.subMeshCount);
+        for (int i = 0; i < mesh.subMeshCount; i++)
+        {
+            triangles = mesh.GetTriangles(i);
+            Debug.Log($"Submesh {i} has {triangles.Length / 3} triangles.");
+        }
+    }*/
 
     void GenerateSubMesh(){
         mesh.subMeshCount = mesh.subMeshCount + 1;
@@ -31,7 +52,7 @@ public class SubmeshManager : EditorWindow
 
     void RemoveSubMesh()
     {
-        mesh.subMeshCount = mesh.subMeshCount - 1;
-        mesh.SetTriangles(mesh.triangles, mesh.subMeshCount - 1);
+        mesh.subMeshCount--;
+        mesh.SetTriangles(mesh.triangles, mesh.subMeshCount);
     }
 }
